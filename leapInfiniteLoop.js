a3a62e17b84f014fce899e8805d15f31a02f7592
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-09-07 23:14:19 -0400

    Update leapInfiniteLoop.js

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -23,7 +23,7 @@ Leap.loop(controllerOptions, function(frame)
     //iterrate
     for(var n = 0; n < arraylength; n++)
     {    
-        console.log(fingers[n].type());
+        console.log(fingers[n].length);
        
     }
     //console.log(fingers);
