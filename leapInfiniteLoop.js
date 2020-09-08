14ad7558969ecd223457205abd8394138ab72410
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-09-07 21:28:04 -0400

    Update leapInfiniteLoop.js
    create random number to affect x in the infinite loop

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -4,9 +4,10 @@ var x = window.innerWidth * 0.5;
 var y = window.innerHeight * 0.5;
 Leap.loop(controllerOptions, function(frame)
 {
+  var rand = (Math.random()*3)-1;
   console.log(i);
   i++;
-  circle(x,y,50);
+  circle(x+rand,y,50);
 
 
 }
