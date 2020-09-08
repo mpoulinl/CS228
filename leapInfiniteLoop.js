91a2948e9a04f2f02a7e93763ab9297b26b98fc4
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-09-07 22:39:02 -0400

    Update leapInfiniteLoop.js

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -15,10 +15,10 @@ Leap.loop(controllerOptions, function(frame)
     var hand = frame.hands[h];
   }
   var hand = frame.hands[0];
-  
+  var fingers = hand.fingers;
   if(frame.hands.length == 1)
   {
-    console.log(hand);
+    console.log(fingers);
     //to acces the first element in the hands array
   }
   
