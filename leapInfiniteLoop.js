0bfd43e84955a40692ba72d1e4fa9ec4f7e3d7ce
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-09-07 21:23:20 -0400

    Update leapInfiniteLoop.js
    Drawing circle, created x y, circle

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,9 +1,12 @@
 var controllerOptions = {};
 var i = 0;
+var x = window.innerWidth * 0.5;
+var y = window.innerHeight * 0.5;
 Leap.loop(controllerOptions, function(frame)
 {
   console.log(i);
   i++;
+  circle(x,y,50);
 
 
 }
