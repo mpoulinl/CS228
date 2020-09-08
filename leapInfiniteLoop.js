794458fa6065be32ebb11bc27e69f82f8aea1d30
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-09-07 21:34:59 -0400

    Update leapInfiniteLoop.js
    comment out everything in infinite loop and added console.log(frame)

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -4,13 +4,13 @@ var x = window.innerWidth * 0.5;
 var y = window.innerHeight * 0.5;
 Leap.loop(controllerOptions, function(frame)
 {
-  clear();
-  var randx = (Math.random()*3)-1;
-  var randy = (Math.random()*3)-1;
-  console.log(i);
-  i++;
-  circle(x+randx,y+randy,50);
-  
+  //clear();
+  //var randx = (Math.random()*3)-1;
+  //var randy = (Math.random()*3)-1;
+  //console.log(i);
+  //i++;
+  //circle(x+randx,y+randy,50);
+  console.log(frame);
 
 
 }
