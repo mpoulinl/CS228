534d053e3b2e4c049540c4962664f4658f235a5f
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-09-07 21:47:21 -0400

    Update leapInfiniteLoop.js
    added a console show in the for loop if one hand show

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -10,9 +10,13 @@ Leap.loop(controllerOptions, function(frame)
   //console.log(i);
   //i++;
   //circle(x+randx,y+randy,50);
-  //console.log(frame);
   for(var h = 0; h < frame.hands.length; h++){
     var hand = frame.hands[h];
+    if(hand == 1)
+    {
+      console.log(frame);
+      //only show in console if one hand
+    }
   }
  
 
