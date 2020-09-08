824832ad22e88112470dea534f6ef336f3d9c326
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-09-07 21:29:32 -0400

    Update leapInfiniteLoop.js
    add clear to take out the trail of old circle

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -4,10 +4,12 @@ var x = window.innerWidth * 0.5;
 var y = window.innerHeight * 0.5;
 Leap.loop(controllerOptions, function(frame)
 {
+  clear();
   var rand = (Math.random()*3)-1;
   console.log(i);
   i++;
   circle(x+rand,y,50);
+  
 
 
 }
