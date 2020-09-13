695b86e3d2afbdefcf739c8d95cc4c19d21e21c9
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-09-13 17:18:35 -0400

    Update leapDrawLines.js

--- leapDrawLines.js
+++ leapDrawLines.js
@@ -49,7 +49,7 @@ function HandleBone(bone,type){
   var end = TransformCoordinates(bone_end[0],bone_end[2])
 
   //line and line weight
-  strokeWeight((type+10)*0.2)
+  strokeWeight((type+5)*0.6)
   stroke((type+10)*0.5)
   line(start[0],start[1],end[0],end[1]);
 
