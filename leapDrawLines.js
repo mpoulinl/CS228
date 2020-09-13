f10b4a363b2d3073cf36da1943b051c28640da2d
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-09-13 11:41:40 -0400

    Update leapDrawLines.js

--- leapDrawLines.js
+++ leapDrawLines.js
@@ -50,11 +50,16 @@ function HandleFinger(finger){
   var x_pos_finger = ((horizontal-rawXMin)/(rawXMax - rawXMin)) * window.innerWidth ;
   var y_pos_finger = window.innerHeight - (((vertical-rawYMin)/(rawYMax-rawYMin))* window.innerHeight);
   console.log(finger.bones[1]);
-
+  for(var n = 0; n < 4; n++){
+    //if index extended
+      HandleBone(finger.bones[n]);
+  }
   //only show circle for index
-      circle(x_pos_finger,y_pos_finger,50);
+  //circle(x_pos_finger,y_pos_finger,50);
 }
-
+function HandleBone(bone){
+  console.log(bone);
+}
 //infinity loop
 Leap.loop(controllerOptions, function(frame){
 
