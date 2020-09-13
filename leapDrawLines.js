3d69cf3882bb502afe732bca916c23ddbdab0349
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-09-13 17:33:34 -0400

    Update leapDrawLines.js

--- leapDrawLines.js
+++ leapDrawLines.js
@@ -85,7 +85,7 @@ function TransformCoordinates(x,y) {
     rawYMax = y;
   }
   var x_pos = (((x-rawXMin)/(rawXMax - rawXMin)) * window.innerWidth) ;
-  var y_pos = ((y-rawYMin)/(rawYMax-rawYMin))* window.innerHeight;
+  var y_pos = window.innerHeight-(((y-rawYMin)/(rawYMax-rawYMin))* window.innerHeight);
   return[x_pos,y_pos];
 }
 //
