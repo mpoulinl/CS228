6a917a3de74f0b180f6c3d88b17b0ccc17b5c186
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-09-13 11:09:57 -0400

    Update leapDrawLines.js

--- leapDrawLines.js
+++ leapDrawLines.js
@@ -51,7 +51,7 @@ function HandleFinger(finger){
 
   var x_pos_finger = ((horizontal-rawXMin)/(rawXMax - rawXMin)) * window.innerWidth ;
   var y_pos_finger = ((vertical-rawYMin)/rawYMax-rawYMin)*window.innerHeight;
-  console.log(rawXMax);
+  console.log(window.innerWidth);
 
   //only show circle for index
       circle(x_pos_finger,y_pos_finger,50);
