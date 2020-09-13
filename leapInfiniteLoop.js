eec275d13a5b9f5ebea59473e0a9f209f25a2e6b
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-09-13 08:51:04 -0400

    Update leapInfiniteLoop.js

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,19 +1,18 @@
 var controllerOptions = {};
 var i = 0;
-var x = window.innerWidth * 0.5;
-var y = window.innerHeight * 0.5;
 
-Leap.loop(controllerOptions, function(frame)
-{
-  clear();
-  //var randx = (Math.random()*3)-1;
-  //var randy = (Math.random()*3)-1;
+
+
+function HandleFrame(frame){
+ clear();
+ 
   for(var h = 0; h < frame.hands.length; h++){
     var hand = frame.hands[h];
-  }
+  }  
+  
   var hand = frame.hands[0];
   
-  if(frame.hands.length == 1)
+   if(frame.hands.length == 1)
   {
     var fingers = hand.fingers;
     var arraylength = fingers.length;
@@ -25,22 +24,21 @@ Leap.loop(controllerOptions, function(frame)
         if(fingers[n].length < 50)
         {
           console.log(fingers[n].tipPosition[0]);
-          circle(fingers[n].tipPosition[0]+x,fingers[n].tipPosition[1]+y,50);
+          circle(fingers[n].tipPosition[0]+(window.innerWidth * 0.5),fingers[n].tipPosition[1]+(window.innerHeight * 0.5),50);
         }
         
       }
        
     }
-
-  //console.log(i);
-  //i++;
-  //circle(x+randx,y+randy,50);
-  //console.log(frame);
-    //console.log(fingers);
-    //to acces the first element in the hands array
-    
   }
   
+ 
+}
+
+Leap.loop(controllerOptions, function(frame)
+{
+ 
+ HandleFrame(frame); 
  
 
 
