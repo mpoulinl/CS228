882a9f40d99eec0515c56da73c1b673a63dacb22
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-09-21 20:51:23 -0400

    Update Playback.js

--- Playback.js
+++ Playback.js
@@ -74,11 +74,9 @@ function draw(){
 
       if(secondframeIndex == 0){
         line(xStart,yStart,zStart,xEnd,yEnd,zEnd);
-        frameIndex++;
       }
       else{
         line(xStart_a,yStart_a,zStart_a,xEnd_a,yEnd_a,zEnd_a);
-        frameIndex++;
       }
       console.log(frameIndex);
 
