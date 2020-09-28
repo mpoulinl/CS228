b34e57c33742ac0cd9614e756c42b7686a35aeb5
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-09-28 10:54:43 -0400

    Update Predict.js

--- Predict.js
+++ Predict.js
@@ -1,10 +1,12 @@
 function Train(){
+  console.log("I am being Trained");
 }
 
 function Test(){
-  
+  console.log("I am being Test");
 }
 function draw(){
   clear();
-  
+  Train();
+  Test();
 }
