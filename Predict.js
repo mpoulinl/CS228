e2cb42769a29033c10599a234458d78fa1606a6c
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-09-28 12:44:03 -0400

    Update Predict.js

--- Predict.js
+++ Predict.js
@@ -164,8 +164,8 @@ function Train(){
     // var int = parseInt(str[8]);
     // console.log(currentFeatures.toString());
     //console.log(currentLabels.toString());
-    //console.log(currentLabels);
-    knnClassifier.addExample(currentFeatures.tolist(),currentLabels)
+    //console.log(currentLabels)
+    knnClassifier.addExample(currentFeatures,currentLabels)
   }
 }
 
