ed79b20d2ea946e63fcfce4f714e53058d248a2d
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-09-28 22:47:05 -0400

    Update Predict.js

--- Predict.js
+++ Predict.js
@@ -154,7 +154,7 @@ irisData = nj.array([[	5.1	,	3.5	,	1.4	,	0.2	,	1	],
 
 var numFeatures = irisData.shape;
 
-predictedClassLabels = nj.zeros(151)
+predictedClassLabels = nj.zeros(150)
 console.log(numFeatures[0]);
 function Train(){
   for(var i = 0 ; i < numFeatures[0]; i = i+2){
