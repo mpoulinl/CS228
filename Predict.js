c56cb8fa0ad3174722b81271a4b5b89fb8b0b422
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-09-28 22:07:57 -0400

    Update Predict.js

--- Predict.js
+++ Predict.js
@@ -202,7 +202,7 @@ function DrawCircles(){
         var l = (currentLabels.get(0))
         var co = l-1
 
-        if(i < 50 && i > 10){
+        if(i < 0){
           circle(x*100,y*100,8)
           fill("red")
           stroke("red")
