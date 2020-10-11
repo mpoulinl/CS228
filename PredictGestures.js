a21f5739d3bfddcf85f163e0ab0c7c994ff423a8
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-10-11 17:23:56 -0400

    Update PredictGestures.js

--- PredictGestures.js
+++ PredictGestures.js
@@ -4,13 +4,6 @@ var testingSampleIndex = 1
 var trainingCompleted = false
 
 
-var numSamples = irisData.shape;
-var numRows = numSamples[0];
-var numFeatures = numSamples[1] - 1;
-predictedClassLabels = nj.zeros(numRows,1)
-
-
-
 function Train(){
   console.log(train0.toString())
 }
