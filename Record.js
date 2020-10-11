327755de638bc905802654fffd07bd429d249092
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-10-11 15:07:23 -0400

    Update Record.js

--- Record.js
+++ Record.js
@@ -19,7 +19,7 @@ function HandleFrame(frame){
   if(frame.hands.length >= 1){
     // first element in the hands
     var hand = frame.hands[0];
-    var InteractionBox = frame.InteractionBox();
+    var InteractionBox = frame.InteractionBox;
     HandleHand(hand,InteractionBox);
 }
 }
