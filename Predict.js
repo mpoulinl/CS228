6f2270aa94da43b3e5ba95620847dd7d4b7c771e
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-10-11 14:10:26 -0400

    Update Predict.js

--- Predict.js
+++ Predict.js
@@ -177,7 +177,7 @@ function Test(){
 }
 
 function GotResults(err, result){
-  console.log(result.label().parseInt());
+  console.log(parseInt(result.label));
   //predictedClassLabels.get(testingSampleIndex) = result.label;
   // testingSampleIndex = testingSampleIndex + 2;
   // if(testingSampleIndex > numRows){
