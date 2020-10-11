2ac2d0e2b3b34b8229bf4f3bd6252d8f07088915
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-10-11 18:17:59 -0400

    Update PredictGestures.js

--- PredictGestures.js
+++ PredictGestures.js
@@ -8,22 +8,26 @@ function Train(){
   //console.log(train0.pick().toString())
   // console.log(test.toString())
   for(var i = 0 ; i < train0.shape[3] ; i++){
-    var features = train0.pick(null,null,null,i)
-    console.log(features.toString())
-    features = features.reshape(1,120)
-    knnClassifier.addExample(features.tolist(),0)
+    var features0 = train0.pick(null,null,null,i)
+    features0 = features0.reshape(1,120)
+
+    var features1 = train1.pick(null,null,null,i)
+    features1 = features1.reshape(1,120)
+
+    knnClassifier.addExample(features0.tolist(),0)
+    knnClassifier.addExample(features1.tolist(),1)
   }
 }
 
 function Test(){
-  var currentFeatures = train0.pick(null,null,null,testingSampleIndex)
+  var currentFeatures = test.pick(null,null,null,testingSampleIndex)
   currentFeatures = currentFeatures.reshape(1,120)
   predictLabel = knnClassifier.classify(currentFeatures.tolist(), GotResults);
 
 }
 
 function GotResults(err, result){
-  
+
   console.log(result.label)
   if(testingSampleIndex >= train0.shape[3]){
     testingSampleIndex = 0;
