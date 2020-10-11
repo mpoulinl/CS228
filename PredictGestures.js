eb8e5b53c4c180c82f65c4c9054f952495af1846
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-10-11 17:39:45 -0400

    Update PredictGestures.js

--- PredictGestures.js
+++ PredictGestures.js
@@ -5,10 +5,13 @@ var trainingCompleted = false
 
 
 function Train(){
-  console.log(train0.pick(1).toString())
+  console.log(train0.pick().toString())
   // console.log(test.toString())
   for(var i = 0 ; i < train0.shape[3] ; i++){
-
+    train0.pick(null,null,null,i).toString()
+  }
+  for(var i = 0 ; i < test.shape[3] ; i++){
+    test.pick(null,null,null,i).toString()
   }
 }
 
