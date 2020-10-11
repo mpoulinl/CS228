704a4a205ddc8933aec7dbd478a56e94e8e2b3c5
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-10-11 14:58:39 -0400

    Update Record.js

--- Record.js
+++ Record.js
@@ -19,7 +19,7 @@ function HandleFrame(frame){
   if(frame.hands.length >= 1){
     // first element in the hands
     var hand = frame.hands[0];
-    HandleHand(hand,InteractionBox);
+    HandleHand(hand,frame.InteractionBox());
 }
 }
 
@@ -44,7 +44,7 @@ function HandleBone(bone,type,fingerIndex,InteractionBox){
   var bone_start = bone.prevJoint;
   var bone_end = bone.nextJoint;
 
-  var normalizedPrevJoint = interactionBox.normalizePoint(bone.prevJoint, true);
+  var normalizedPrevJoint = InteractionBox.normalizePoint(bone.prevJoint, true);
 
   console.log(normalizedPrevJoint);
   var start = TransformCoordinates(bone_start[0],bone_start[1])
