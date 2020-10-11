89ae62ab1e7bf98d0b7d3c2754058933d15b56d3
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-10-11 17:50:26 -0400

    Update PredictGestures.js

--- PredictGestures.js
+++ PredictGestures.js
@@ -8,11 +8,16 @@ function Train(){
   //console.log(train0.pick().toString())
   // console.log(test.toString())
   for(var i = 0 ; i < train0.shape[3] ; i++){
-    console.log(train0.pick(null,null,null,i).toString())
+    var features = train0.pick(null,null,null,i)
+    features = features.reshape(1,120)
+    //knnClassifier.addExample(features.tolist(),currentLabel)
   }
-  for(var i = 0 ; i < test.shape[3] ; i++){
-    console.log(test.pick(null,null,null,i).toString())
-  }
+  // var currentFeatures = irisData.pick(i).slice([0,4])
+  //   var currentLabel = irisData.pick(i).get(4)
+  //   knnClassifier.addExample(currentFeatures.tolist(),currentLabel)
+  // for(var i = 0 ; i < test.shape[3] ; i++){
+  //   var features = test.pick(null,null,null,i)
+  // }
 }
 
 function Test(){
