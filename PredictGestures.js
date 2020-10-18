71bd9dad3532dc0e2542da74bdf6d118ffdbfe55
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-10-18 19:39:29 -0400

    4

--- PredictGestures.js
+++ PredictGestures.js
@@ -36,6 +36,10 @@ function Train(){
     features = features.reshape(1,120)
     knnClassifier.addExample(features.tolist(),4)
 
+    features = train4two.pick(null,null,null,i)
+    features = features.reshape(1,120)
+    knnClassifier.addExample(features.tolist(),4)
+
     features = train5.pick(null,null,null,i)
     features = features.reshape(1,120)
     knnClassifier.addExample(features.tolist(),5)
