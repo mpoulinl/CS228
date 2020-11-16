da5b7434ebf8bccf890512b89c7a42d18933d69f
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-11-16 15:26:57 -0500

    all digit

--- prepareToDraw.js
+++ prepareToDraw.js
@@ -19,9 +19,16 @@ arrowDown = loadImage('https://i.imgur.com/VlCRuC0.jpg');
 arrowToward = loadImage('https://i.imgur.com/0ShbKCa.jpg');
 arrowAway = loadImage('https://i.imgur.com/Lgjl8YM.jpg');
 
+asl_0 = loadImage('https://i.imgur.com/MMulLzG.jpg');
 asl_1 = loadImage('https://i.imgur.com/gs1Emzq.jpg');
 asl_2 = loadImage('https://i.imgur.com/AVxiMC7.png');
 asl_3 = loadImage('https://i.imgur.com/1MoLVrk.jpg');
+asl_4 = loadImage('https://i.imgur.com/dqcMzpA.jpg');
+asl_5 = loadImage('https://i.imgur.com/r9xMM7F.jpg');
+asl_6 = loadImage('https://i.imgur.com/WMy4ptk.jpg');
+asl_7 = loadImage('https://i.imgur.com/Ss5FJr2.jpg');
+asl_8 = loadImage('https://i.imgur.com/iqr7Nto.jpg');
+asl_9 = loadImage('https://i.imgur.com/NmfkDHf.jpg');
 
 
 }
