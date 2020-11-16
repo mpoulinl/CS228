cb526aacb27f7fbdf8625769eaf474b2b336590a
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-11-16 15:12:47 -0500

    cancel train 7 four

--- PredictGestures.js
+++ PredictGestures.js
@@ -157,9 +157,6 @@ function Train(){
     features = features.reshape(1,120)
     knnClassifier.addExample(features.tolist(),7)
 
-    features = train7four.pick(null,null,null,i)
-    features = features.reshape(1,120)
-    knnClassifier.addExample(features.tolist(),7)
 
 
     features = train8.pick(null,null,null,i)
