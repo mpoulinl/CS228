f16b207ecc132904e34d35f7a1416c9152edd479
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-11-30 20:10:00 -0500

    Update PredictGestures.js

--- PredictGestures.js
+++ PredictGestures.js
@@ -77,7 +77,7 @@ function SignIn(){
 
   //paragraph
   var user_playing = document.getElementById("user-play");
-  user_playing.innerHTML = String(username);
+  user_playing.innerHTML = String(username + "hi");
   if(IsNewUser(username,list)){
     CreateNewUser(username,list)
     //new list sign in
