7e54cac7c206fd6e9c8f65a9bfdffecb2a91cf34
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-12-07 20:14:14 -0500

    Update PredictGestures.js

--- PredictGestures.js
+++ PredictGestures.js
@@ -33,7 +33,7 @@ function CreateNewUser(username,list){
     vec_name = [username];
   }
   else{
-    vec_name.appendChild(username);
+    vec_name.apped(username);
   }
   console.log(vec_name);
   num_user++;
