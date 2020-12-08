1a45856149ffee9b9d5326644723f4dc8b7ce5fc
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-12-07 19:11:47 -0500

    Update PredictGestures.js

--- PredictGestures.js
+++ PredictGestures.js
@@ -103,7 +103,8 @@ function SignIn(){
       user_playing.innerHTML = String("User : " + username + "    Last Score: " + document.getElementById(username+"_signins").value);
       console.log(username);
       console.log( document.getElementById(username+"_signins").value)
-      console.log( document.getElementById(username+"_signins"))
+      console.log( str(document.getElementById(username+"_signins").value))
+      console.log(str(document.getElementById(username+"_signins")))
 
   }
 
