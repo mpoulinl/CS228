d439147594d59385258d2eeb60c0a4eb51017ec9
mpoulinl <70707326+mpoulinl@users.noreply.github.com>
2020-12-07 21:04:21 -0500

    Update PredictGestures.js

--- PredictGestures.js
+++ PredictGestures.js
@@ -75,6 +75,7 @@ function IsNewUser(username,list){
 
 }
 function ranking(){
+  console.log("length ",num_user.length);
   for(var i = 0 ; i < num_user.length ; i++){
     document.getElementById(username+"_signins").innerHTML
     var initial = i;
